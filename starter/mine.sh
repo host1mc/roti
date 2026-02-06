c1312c5cf28ee066c4cc4ca738867724acadcaf4
host1mc <33aryan10a@gmail.com>
2026-02-06 17:27:01 +0530

    Update mine.sh

--- starter/mine.sh
+++ starter/mine.sh
@@ -1,1 +1,14 @@
 
+#!/bin/bash
+
+echo "=============================="
+echo "     STARTING MINECRAFT"
+echo "=============================="
+echo
+
+cd MinecraftServer || {
+  echo "❌ MinecraftServer directory not found!"
+  exit 1
+}
+
+java -Xmx2G -Xms1G -jar server.jar nogui
