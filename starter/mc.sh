259883f61595b2baff1a709dfe755fe291f01e09
host1mc <33aryan10a@gmail.com>
2026-02-06 17:25:41 +0530

    Update mc.sh

--- starter/mc.sh
+++ starter/mc.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
+clear
 echo "=============================="
 echo "        ROTI BHOSDI"
 echo "=============================="
 echo
+echo "Choose an option:"
+echo "1) Run VM Starter"
+echo "2) Run Second Script"
+echo
+read -p "Enter your choice (1 or 2): " choice
+echo
 
-bash <(curl -s https://raw.githubusercontent.com/host1mc/vmstarter/refs/heads/main/vm.sh)
+case $choice in
+  1)
+    echo "Running first script..."
+    bash <(curl -s https://raw.githubusercontent.com/host1mc/vmstarter/refs/heads/main/vm.sh)
+    ;;
+  2)
+    echo "Start Minecraft "
+    bash <(curl -s https://raw.githubusercontent.com/host1mc/roti/refs/heads/main/starter/mine.sh )
+    ;;
+  *)
+    echo "❌ Invalid choice. Please enter 1 or 2."
+    ;;
+esac
