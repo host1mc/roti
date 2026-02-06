ae1bbe59d870e53e1b194c2070604dc88de228d7
host1mc <33aryan10a@gmail.com>
2026-02-06 17:22:29 +0530

    Create mc.sh

---
+++ starter/mc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "=============================="
+echo "        ROTI BHOSDI"
+echo "=============================="
+echo
+
+bash <(curl -s https://raw.githubusercontent.com/host1mc/vmstarter/refs/heads/main/vm.sh)
